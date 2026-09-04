8d2dd2ffc79bf57471f6d47bd5b7ccd8d0f0d125
Federico-Rosso <contacto@revoltagenciacreativa.com.ar>
2026-09-04 10:11:53 -0300

    Update Portfolio.jsx

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,5 +1,5 @@
 import React, { useMemo } from 'react';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 import { motion } from 'framer-motion';
 import ProjectCard from './ProjectCard';
 import { desarrollos } from '../data/desarrollos';
